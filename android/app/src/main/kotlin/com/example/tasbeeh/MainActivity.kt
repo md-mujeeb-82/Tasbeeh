aff4c60be6b6a3025c07c8d3fe45f2b1f71f415a
Mujeeb Mohammad <mohammad.mujeeb@gmail.com>
2026-03-25 09:08:20 +0530

    Bug fix with AudioHander.

--- android/app/src/main/kotlin/com/example/tasbeeh/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tasbeeh/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.tasbeeh
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : AudioServiceActivity()
